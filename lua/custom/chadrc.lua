855d960af532564f9d8f1199fbfcc4c3c6c3f224
Milan Hommet <mhommet@teicee.com>
2024-03-18 17:04:50 +0100

    change ui

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -4,9 +4,13 @@ local M = {}
 M.ui = {
   theme = 'catppuccin',
   transparency = true,
-  nvdash = {
-    load_on_startup = true,
-  }
+  extended_integrations={'notify'},
+  telescope = { style = "borderless" },
+  statusline = {
+    theme = "minimal",
+    separator_style = "round",
+    overriden_modules = nil,
+  },
 }
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
