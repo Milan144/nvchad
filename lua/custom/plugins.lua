c1d75011edb33924af1d8548e7d46ec94ef0b8e7
milan144 <milan.hommet@protonmail.com>
2024-03-22 22:18:21 +0100

    adding chatgpt

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -132,5 +132,19 @@ local plugins = {
       return opts
     end
   },
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function ()
+      require("chatgpt").setup({
+        api_key_cmd = "OPENAI_API_KEY",
+      })
+    end,
+  },
 }
 return plugins
